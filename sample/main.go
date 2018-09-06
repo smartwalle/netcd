c3c68d2f4bcff40b6e1cef90f9f8bed3be2c8161
SmartWalle <smartwalle@gmail.com>
2018-09-06 17:28:16 +0800

    update

--- sample/main.go
+++ sample/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/coreos/etcd/clientv3"
 	"github.com/smartwalle/etcd4go"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -16,10 +15,7 @@ func main() {
 	config.Endpoints = []string{"localhost:2379"}
 
 	var s, _ = etcd4go.NewService(config)
-	fmt.Println(s.Register("my_service", "service1", "123", 5))
-	time.Sleep(time.Second * 5)
-	//fmt.Println(s.Revoke("my_service", "service1"))
-	fmt.Println(s.Register("my_service", "service2", "456", 5))
+	fmt.Println(s.Register("my_service", "node_1", "123", 5))
 
 	wg.Wait()
 }
