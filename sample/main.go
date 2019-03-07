76cb0efe69c439702575afe919c3e8bcfb28ef64
SmartWalle <smartwalle@gmail.com>
2019-03-07 20:41:49 +0800

    rename method

--- sample/main.go
+++ sample/main.go
@@ -15,7 +15,7 @@ func main() {
 	config.Endpoints = []string{"localhost:2379"}
 
 	var c, _ = etcd4go.NewClient(config)
-	id, key, err := c.Register("my_service", "node_1", "123", 5)
+	id, key, err := c.Register("my_service/node_1", "123", 5)
 	fmt.Println(id, key, err)
 
 	wg.Wait()
